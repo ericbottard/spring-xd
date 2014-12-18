65278e8341cf2bf97c09bc83934a83942a0f1c6f
Eric Bottard <ebottard@gopivotal.com>
2014-12-18 17:19:43 +0100

    XD-1097: Enable flexible module CL patterns

--- spring-xd-module/src/main/java/org/springframework/xd/module/support/ModuleUtils.java
+++ spring-xd-module/src/main/java/org/springframework/xd/module/support/ModuleUtils.java
@@ -21,6 +21,7 @@ package org.springframework.xd.module.support;
 import java.io.File;
 import java.io.IOException;
 import java.net.URL;
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.boot.loader.archive.Archive;
@@ -29,6 +30,7 @@ import org.springframework.boot.loader.archive.JarFileArchive;
 import org.springframework.boot.loader.util.AsciiBytes;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.io.Resource;
+import org.springframework.core.io.support.ResourcePatternResolver;
 import org.springframework.util.Assert;
 import org.springframework.xd.module.SimpleModuleDefinition;
 
@@ -47,19 +49,21 @@ public class ModuleUtils {
 			File moduleFile = moduleLocation.getFile();
 			Archive moduleArchive = moduleFile.isDirectory() ? new ExplodedArchive(moduleFile) : new JarFileArchive
 					(moduleFile);
-			List<Archive> nestedArchives = moduleArchive.getNestedArchives(new Archive.EntryFilter() {
-				@Override
-				public boolean matches(Archive.Entry entry) {
-					return !entry.isDirectory() && entry.getName().startsWith(LIB);
+
+			String[] patterns = new String[] {"/lib/*.jar"};
+
+			ResourcePatternResolver resolver = new ArchiveResourceLoader(moduleArchive);
+
+			List<URL> urls = new ArrayList<URL>();
+			for (String pattern : patterns) {
+				for (Resource jar : resolver.getResources(pattern)) {
+					urls.add(jar.getURL());
 				}
-			});
-			URL[] urls = new URL[nestedArchives.size() + 1];
-			int i = 0;
-			for (Archive nested : nestedArchives) {
-				urls[i++] = nested.getUrl();
 			}
-			urls[i] = moduleArchive.getUrl();
-			return new ParentLastURLClassLoader(urls, parent);
+
+			// Add the module archive itself
+			urls.add(moduleArchive.getUrl());
+			return new ParentLastURLClassLoader(urls.toArray(new URL[urls.size()]), parent);
 		}
 		catch (IOException e) {
 			throw new RuntimeException(e);
