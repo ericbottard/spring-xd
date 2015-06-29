0ced6a885182731404108d088d212793801eeb7f
Eric Bottard <ebottard@gopivotal.com>
2015-06-29 15:06:26 +0200

    Fix some tests

---
+++ spring-xd-analytics/src/test/java/org/springframework/xd/analytics/metrics/influxdb/InfluxDBCounterRepositoryTests.java
@@ -0,0 +1,31 @@
+package org.springframework.xd.analytics.metrics.influxdb;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.runner.RunWith;
+
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.xd.analytics.metrics.AbstractCounterRepositoryTests;
+import org.springframework.xd.analytics.metrics.common.InfluxDBRepositoriesConfig;
+import org.springframework.xd.test.influxdb.InfluxDBTestSupport;
+
+/**
+ */
+@ContextConfiguration(classes = InfluxDBRepositoriesConfig.class)
+@RunWith(SpringJUnit4ClassRunner.class)
+
+public class InfluxDBCounterRepositoryTests extends AbstractCounterRepositoryTests {
+
+
+	@Rule
+	public InfluxDBTestSupport influxDBAvailableRule = new InfluxDBTestSupport();
+
+	@Before
+	@After
+	public void cleanUp() {
+		counterRepository.deleteAll();
+	}
+
+}
